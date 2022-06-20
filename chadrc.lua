9ed37b3714b6c73ac980e8f40dcd3a2aeb1f1318
jixuecao <2386900869@qq.com>
2022-06-20 11:30:52 +0800

    Update chadrc.lua

--- chadrc.lua
+++ chadrc.lua
@@ -35,6 +35,17 @@ M.plugins = {
 M.options = {
    user = function()
       vim.opt.scrolloff = 3
+      
+      -- 参考: https://zhuanlan.zhihu.com/p/51017142
+      -- tabstop 选项只修改 tab 字符的显示宽度，不修改按 Tab 键的行为
+      vim.opt.tabstop = 4
+      -- softtabstop 选项修改按 Tab 键的行为，不修改 tab 字符的显示宽度。具体行为跟 tabstop 选项值有关
+      vim.opt.softtabstop = 4
+      -- shiftwidth 换行时的自动缩进列数
+      vim.opt.shiftwidth = 4
+      -- expandtab 选项把插入的 tab 字符替换成特定数目的空格。具体空格数目跟 tabstop 选项值有关
+      vim.opt.expandtab = true
+
    end,
 }
 
